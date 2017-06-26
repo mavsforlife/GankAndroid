df01b20f77000fdbd678e4ba3ab0e198a6b18097
Mavs <aqwwm1991@126.com>
2017-06-26 15:54:21 +0800

    add new activity

--- app/src/main/java/com/victor/gankandroid/activity/MainActivity.java
+++ app/src/main/java/com/victor/gankandroid/activity/MainActivity.java
@@ -1,16 +1,10 @@
 package com.victor.gankandroid.activity;
 
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RemoteViews;
 import android.widget.Toast;
 
 import com.victor.gankandroid.R;
@@ -19,17 +13,20 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends Activity implements View.OnClickListener {
-
+public class MainActivity extends Activity {
     @BindView(R.id.tv_android)
     Button mTvAndroid;
     @BindView(R.id.tv_ios)
-    Button mTviOS;
+    Button mTvIos;
     @BindView(R.id.tv_fore)
     Button mTvFore;
 
-
-    //TextView mTvAndroid, mTvIOs, mTvFore;
+//    @BindView(R.id.tv_android)
+//    Button mTvAndroid;
+//    @BindView(R.id.tv_ios)
+//    Button mTviOS;
+//    @BindView(R.id.tv_fore)
+//    Button mTvFore;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,7 +36,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     }
 
     @OnClick({R.id.tv_android, R.id.tv_ios, R.id.tv_fore})
-    public void onClick(View view) {
+    public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.tv_android:
                 Toast.makeText(this, "android", Toast.LENGTH_SHORT).show();
@@ -47,7 +44,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 break;
             case R.id.tv_ios:
                 Toast.makeText(this, "iOS", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(MainActivity.this, DetailListActivity.class));
+                startActivity(new Intent(MainActivity.this, SoundActivity.class));
                 break;
             case R.id.tv_fore:
                 Toast.makeText(this, "fore", Toast.LENGTH_SHORT).show();
@@ -55,5 +52,23 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 break;
         }
     }
+
+//    @OnClick({R.id.tv_android, R.id.tv_ios, R.id.tv_fore})
+//    public void onClick(View view) {
+//        switch (view.getId()) {
+//            case R.id.tv_android:
+//                Toast.makeText(this, "android", Toast.LENGTH_SHORT).show();
+//                startActivity(new Intent(MainActivity.this, DetailListActivity.class));
+//                break;
+//            case R.id.tv_ios:
+//                Toast.makeText(this, "iOS", Toast.LENGTH_SHORT).show();
+//                startActivity(new Intent(MainActivity.this, SoundActivity.class));
+//                break;
+//            case R.id.tv_fore:
+//                Toast.makeText(this, "fore", Toast.LENGTH_SHORT).show();
+//                startActivity(new Intent(MainActivity.this, DetailListActivity.class));
+//                break;
+//        }
+//    }
 
 }
