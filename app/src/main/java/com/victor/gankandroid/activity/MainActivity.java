b1e79b5a3738b150c4d9aaac6d8bc5a724a9aeba
Mavs <aqwwm1991@126.com>
2017-06-28 14:09:53 +0800

    fomat

--- app/src/main/java/com/victor/gankandroid/activity/MainActivity.java
+++ app/src/main/java/com/victor/gankandroid/activity/MainActivity.java
@@ -14,19 +14,13 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends Activity {
+
     @BindView(R.id.tv_android)
     Button mTvAndroid;
     @BindView(R.id.tv_ios)
     Button mTvIos;
     @BindView(R.id.tv_fore)
     Button mTvFore;
-
-//    @BindView(R.id.tv_android)
-//    Button mTvAndroid;
-//    @BindView(R.id.tv_ios)
-//    Button mTviOS;
-//    @BindView(R.id.tv_fore)
-//    Button mTvFore;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -52,23 +46,4 @@ public class MainActivity extends Activity {
                 break;
         }
     }
-
-//    @OnClick({R.id.tv_android, R.id.tv_ios, R.id.tv_fore})
-//    public void onClick(View view) {
-//        switch (view.getId()) {
-//            case R.id.tv_android:
-//                Toast.makeText(this, "android", Toast.LENGTH_SHORT).show();
-//                startActivity(new Intent(MainActivity.this, DetailListActivity.class));
-//                break;
-//            case R.id.tv_ios:
-//                Toast.makeText(this, "iOS", Toast.LENGTH_SHORT).show();
-//                startActivity(new Intent(MainActivity.this, SoundActivity.class));
-//                break;
-//            case R.id.tv_fore:
-//                Toast.makeText(this, "fore", Toast.LENGTH_SHORT).show();
-//                startActivity(new Intent(MainActivity.this, DetailListActivity.class));
-//                break;
-//        }
-//    }
-
 }
