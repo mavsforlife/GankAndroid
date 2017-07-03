3e4c7a3ecbe9741488e22a8bd9b7c63dc58904cb
Mavs <aqwwm1991@126.com>
2017-07-03 16:09:33 +0800

    test solid toast

--- app/src/main/java/com/victor/gankandroid/activity/SoundActivity.java
+++ app/src/main/java/com/victor/gankandroid/activity/SoundActivity.java
@@ -1,15 +1,40 @@
 package com.victor.gankandroid.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.victor.gankandroid.R;
+import com.victor.gankandroid.widget.SolidToast;
+import com.victor.gankandroid.widget.SoundView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SoundActivity extends AppCompatActivity {
+
+    @BindView(R.id.sound_view)
+    SoundView mSoundView;
+    @BindView(R.id.btn_toast)
+    Button mBtnToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_toast)
+    public void onViewClicked() {
+        SolidToast toast = new SolidToast.Builder()
+                .context(this)
+                .animation(R.style.custom_toast_anim_view)
+                .text("test")
+                .duration(Toast.LENGTH_SHORT)
+                .build();
+        toast.show();
     }
 }
